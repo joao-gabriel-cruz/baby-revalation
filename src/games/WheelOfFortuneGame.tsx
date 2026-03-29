664641ab89f5b4bef182a49360850a44c7cd6b69
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2026-03-29 08:05:22 -0300

    :bug:

--- src/games/WheelOfFortuneGame.tsx
+++ src/games/WheelOfFortuneGame.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useRef } from 'react'
+import { useState, useCallback } from 'react'
 import type { WheelRound, RoundState } from '../types'
 import './WheelOfFortuneGame.css'
 
